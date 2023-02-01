5554a32d0256f2bcd52c8fec72cf4198c64835a5
mkswagger <mathangykrishna16@gmail.com>
2023-02-01 20:01:55 +0530

    nested

---
+++ week1_basics/nestedloop.js
@@ -0,0 +1,7 @@
+//multiple tasks at once
+//printing days of the week with week and day count
+for(var week=1;week<=2;week++){
+    for(var day=1;day<=7;day++){
+        console.log("week:",week,"day:",day)
+    }
+}
---
+++ week1_basics/nestedloop_ex.js
@@ -0,0 +1,5 @@
+var cubes='ABCDEFG';
+for(var i=0;i<cubes.length;i++){
+    var styles="font-size:40 px;border-radius :10px ; border :1 px solid blue; background: blue; color:purple;"
+    console.log("%c"+cubes[i],styles)
+}
