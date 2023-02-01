a6d77463919e2faf8a4f720a50ff59c27ff34cc9
mkswagger <mathangykrishna16@gmail.com>
2023-02-01 22:00:36 +0530

    Update nestedloop_ex.js

--- week1_basics/nestedloop_ex.js
+++ week1_basics/nestedloop_ex.js
@@ -1,5 +1,9 @@
-var cubes='ABCDEFG';
-for(var i=0;i<cubes.length;i++){
-    var styles="font-size:40 px;border-radius :10px ; border :1 px solid blue; background: blue; color:purple;"
-    console.log("%c"+cubes[i],styles)
+newFunction();
+
+function newFunction() {
+    var cubes = 'ABCDEFG';
+    for (var i = 0; i < cubes.length; i++) {
+        var styles = "font-size:40 px;border-radius :10px ; border :1 px solid blue; background: blue; color:purple;";
+        console.log("%c" + cubes[i], styles);
+    }
 }
