8703196beff43479af17ea7906ea0605bfae38be
mkswagger <mathangykrishna16@gmail.com>
2023-03-05 14:28:06 +0530

    Create objects.js

---
+++ week2/objects.js
@@ -0,0 +1,11 @@
+var storemanager={}//object created
+storemanager.socialskills=30
+storemanager.health=30
+storemanager.greeting="hello"
+//each trait is a property of the object
+
+var assmanager={
+    movement:3,
+    skills:20,
+    health:40
+}
