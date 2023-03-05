3e373b26589ac982201b8a8464884cc0c637fbd9
mkswagger <mathangykrishna16@gmail.com>
2023-03-05 14:18:24 +0530

    Update arrays.js

--- week2/arrays.js
+++ week2/arrays.js
@@ -1,2 +1,11 @@
 var train1=["wheat","barley","potato","salt","rocks"]
 console.log(train1[0])
+
+function listarray(arr){
+    for(var i=0;i<arr.length;i++){
+        console.log(i,arr[i])
+    }
+}
+
+var colors=["red","orange","yellow","green","blue"]
+listarray(colors)
