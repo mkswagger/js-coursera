ba6676554160a5f030b6601eff8dc027a96c48eb
mkswagger <mathangykrishna16@gmail.com>
2023-03-05 18:35:27 +0530

    add obj

---
+++ week2/arrayasobj.js
@@ -0,0 +1,8 @@
+//push() and pop() method
+
+var fruits=[]
+fruits.push("apple")
+fruits.push("mango")
+console.log(fruits)
+fruits.pop()//last element
+console.log(fruits)
---
+++ week2/mathobj.js
@@ -0,0 +1,14 @@
+//builtin object called math
+//math.pi =3.1416
+//math.e=2.71
+//math.ln2= log 2=0.693
+
+//rounding methods
+//math.ceil()-max value
+//math.floor()-lower value
+//math.rouund() - closest decimal
+//math.trunc() -truncates the decimal , only integer
+//math.pow(2,3)=2^3=8
+//math.sqrt(36)=6
+//math.cbrt(8)=2
+//math.abs(-10)=10
