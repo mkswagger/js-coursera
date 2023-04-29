6ee6b74d1982d8401fbbee8ff766d1eae786e9f8
mkswagger <mathangykrishna16@gmail.com>
2023-04-29 23:56:44 +0530

    assignment week 2

---
+++ week2/arraysobject_assign.js
@@ -0,0 +1,16 @@
+var clothes=[];
+clothes.push("jeans");
+clothes.push("hat");
+clothes.push("jacket");
+clothes.push("tees");
+clothes.push("shirts");
+clothes.pop();
+console.log(clothes);
+
+
+//object qn
+
+var favcar={};
+favcar.color= "red";
+favcar.convertible= true;
+console.log(favcar);
