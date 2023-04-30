2731449cae15a247188dd4a90de070845baebc6b
mkswagger <mathangykrishna16@gmail.com>
2023-04-30 12:51:06 +0530

    errors and bugs intro

---
+++ week2/error handling/errorsnbugs.js
@@ -0,0 +1,11 @@
+//error is code that prevents the program from stoping
+
+//syntax, type, reference error
+
+//syntax error -  not following the rules of js
+
+//var word = "hello; 
+
+//type error -data type error
+
+// (5).po;
