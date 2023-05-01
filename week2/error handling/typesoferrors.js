8fe47409916bfc12abb503d18d3bfa765267aa89
mkswagger <mathangykrishna16@gmail.com>
2023-05-01 13:15:44 +0530

    types of errors

---
+++ week2/error handling/typesoferrors.js
@@ -0,0 +1,26 @@
+//Reference Error
+// when one tries to call a variable which is not declared anywhere
+//console.log(username); 
+
+//Synatx Error
+
+//any kind of invalid js code will cause syntax error
+
+// var a "theres no op here";
+
+//Type Error
+
+// trying to run a non supported datatype
+
+// "hwllo".pop()
+
+//Range Error
+
+//  out of range of acceptable input values
+
+console.log((10).toString(2));//converted dec to bin
+console.log((10).toString(8)); //converted dec to hex
+
+console.log((10).toString(100)); // range error coz base 100 is out of range
+//for no systems that are available to tostring()
+
