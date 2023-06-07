356fc317f61b300aac33e32bca1cec667482a761
mkswagger <mathangykrishna16@gmail.com>
2023-06-07 16:55:11 +0530

    Create var _let_const.js

---
+++ week3/functionalprgmin/var _let_const.js
@@ -0,0 +1,18 @@
+// ES5 JS has functions in local scope
+//ES6 js has functions in block scope
+//block scope - variable declared in that block of code is accesible inside the block alone
+//let and cons - block scope
+
+// char of var keyword
+
+// can use it before it is even declared
+//same variable can be redeclared
+//can be scoped to a function globallyy
+
+//let and const more strict
+
+//cant be used before it is declared
+//variable cany be redeclared
+//its scope is to the block
+
+//use let if the variable changes and const if its fixed
