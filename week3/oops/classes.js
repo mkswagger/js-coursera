24252d541caf6f33f4e74224fc1a931dbda41d65
mkswagger <mathangykrishna16@gmail.com>
2023-06-07 18:25:58 +0530

    oops concepts

---
+++ week3/oops/classes.js
@@ -0,0 +1,9 @@
+class Car{
+    constructor(color, speed){
+        this.color = color;
+        this.speed = speed;
+    }
+    turbooon(){
+        console.log("turbo is on");
+    }
+}
---
+++ week3/oops/oopsintro.js
@@ -0,0 +1,25 @@
+//functions inside objects are methods
+//FP APPROACH
+var shoes= 100;
+var statetax=1.2;
+
+function totalprice(shoes, tax){
+    return shoes*tax;
+}
+var topay = totalprice(shoes, statetax);
+
+console.log(topay);
+
+//OOP APPROACH
+
+var purchase1={
+    shoes: 100,
+    statetax:1.2,
+    totalprice : function(){
+        var calculation = purchase1.shoes * purchase1.statetax;
+        console.log(calculation);
+    }
+}
+
+purchase1.totalprice();
+//code resuability is high in objects 
