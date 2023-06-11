05dad26c1c85bd8488dd8ff54b8911402ebdf867
mkswagger <mathangykrishna16@gmail.com>
2023-06-11 12:24:22 +0530

    Update templateiterals.js

--- week3/advancedjs/templateiterals.js
+++ week3/advancedjs/templateiterals.js
@@ -1,3 +1,14 @@
 var greet= "watha";
 var nam= "sekaruu";
 console.log(greet+"!"+ nam );
+//ES5 Standards
+
+let noMultiLine = "No multi-line in ES5";
+console.log(noMultiLine);
+
+//ES6 Standard
+
+// let multiLine = '
+// ya mannn 
+// lets cook
+//  meth';
