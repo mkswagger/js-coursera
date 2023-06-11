ff1fed5d7a9e532db3c10907f787de9daab762b7
mkswagger <mathangykrishna16@gmail.com>
2023-06-11 12:07:48 +0530

    Create forloopsandobj.js

---
+++ week3/advancedjs/forloopsandobj.js
@@ -0,0 +1,16 @@
+// const Car={
+//     speed:100,
+//     color:'blue',
+// }
+
+// for(prop of Car){
+//       console.log(prop)
+// } car is not iterable
+
+ const colors=['red','green','yellow']
+ for(var i in colors){
+    console.log(i);
+ }
+ // object.keys(),object.values(),object.entries()
+
+ 
